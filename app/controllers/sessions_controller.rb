392738d7e62b9264c484ac93dad425524d5422f6
kylesallette <kylesallette@gmail.com>
2018-02-11 21:02:04 -0700

    got new user and log in working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
 
   def create
-    @user = User.find_by(email: params[:email])
+    @user = User.find_by(name: params[:name])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
 
   def new
 
-  end 
+  end
 
   def destroy
     session[:user_id] = nil
