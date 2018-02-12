45fd68950cde102ed3e779f6155f6395fe8c2fec
kylesallette <kylesallette@gmail.com>
2018-02-12 08:48:42 -0700

    tweaking sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, :notice => "Logged out"
+    redirect_to root_path, :notice => "Successfully logged out"
   end
 
 end
