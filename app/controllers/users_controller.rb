7446527bd863fa3c41e83feb080ee10c8927867a
kylesallette <kylesallette@gmail.com>
2018-02-13 08:05:42 -0700

    some buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] = "<b>user created successfully.</b>"
+      flash[:success] = "User created successfully."
       redirect_to user_path(@user)
     else
       render :new
